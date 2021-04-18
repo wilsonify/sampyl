6a983f70dee2d8f29c674d16edbc633f6949f15c
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-04-18 10:43:32 -0400

    whitespace

--- src/setup.py
+++ src/setup.py
@@ -5,25 +5,27 @@ import setuptools
 with open("../README.md", "r") as fh:
     long_description = fh.read()
 
+classifiers = [
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Operating System :: OS Independent'
+]
 
-classifiers = ['Programming Language :: Python',
-               'Programming Language :: Python :: 2',
-               'Programming Language :: Python :: 3',
-               'Programming Language :: Python :: 2.7',
-               'Programming Language :: Python :: 3.6',
-               'License :: OSI Approved :: MIT License',
-               'Intended Audience :: Science/Research',
-               'Topic :: Scientific/Engineering',
-               'Topic :: Scientific/Engineering :: Mathematics',
-               'Operating System :: OS Independent']
-
-
-setuptools.setup(name='sampyl-mcmc',
-                 version='0.3',
-                 description='MCMC Samplers in Python & Numpy',
-                 author='Mat Leonard',
-                 author_email='leonard.mat@gmail.com',
-                 url='http://matatat.org/sampyl/',
-                 packages=['sampyl', 'sampyl.samplers'],
-                 classifiers=classifiers,
-                 install_requires=['numpy', 'scipy', 'autograd'])
+setuptools.setup(
+    name='sampyl-mcmc',
+    version='0.3',
+    description='MCMC Samplers in Python & Numpy',
+    author='Mat Leonard',
+    author_email='leonard.mat@gmail.com',
+    url='http://matatat.org/sampyl/',
+    packages=['sampyl', 'sampyl.samplers'],
+    classifiers=classifiers,
+    install_requires=['numpy', 'scipy', 'autograd']
+)
